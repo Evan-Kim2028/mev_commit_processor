83feac76a779880ebf0997fdebdb1dbaf8b96c46
Evan-Kim2028 <ekcopersonal@gmail.com>
2024-12-05 16:14:08 -0500

    fix: add eth context

--- src/validatorRegistryProcessor.ts
+++ src/validatorRegistryProcessor.ts
@@ -1,13 +1,12 @@
-import { EthChainId } from "@sentio/sdk/eth";
+import { EthChainId, EthContext } from "@sentio/sdk/eth";
 import { VanillaRegistryProcessor } from './types/eth/vanillaregistry.js';
 
 export function initVanillaRegistryProcessor() {
   VanillaRegistryProcessor.bind({
-    address: '0x87D5F694fAD0b6C8aaBCa96277DE09451E277Bcf',
-    // network: EthChainId.METIS, // overwrite Metis network with mev-commit testnet chain id in sentio.yaml
+    address: '0x87D5F694fAD0b6C8aaBCa96277DE09451E277Bcf', // This is the contract address on Holesky
     network: EthChainId.HOLESKY
   })
-    .onEventStakeAdded(async (event, ctx) => {
+    .onEventStakeAdded(async (event, ctx: EthContext) => {
       ctx.eventLogger.emit('StakeAdded', {
         amount: event.args.amount.toString(),
         valBLSPubKey: event.args.valBLSPubKey,
